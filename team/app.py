77625ca8b8d7102a1cd4faedf34b21391dffc510
shukurimu0121 <118036362+shukurimu0121@users.noreply.github.com>
2023-03-06 21:40:23 +0900

    cssが適用されるようになりました。また、お気に入り登録機能も少し進めました。そして、いらなくなったhtmlファイルを削除しました。

--- team/app.py
+++ team/app.py
@@ -5,9 +5,10 @@ from flask import Flask, flash, jsonify, redirect, render_template, request, ses
 from flask_session import Session
 from functools import wraps
 from werkzeug.security import check_password_hash, generate_password_hash
+import json
 
 # Configure application
-app = Flask(__name__, static_folder='./templates/images')
+app = Flask(__name__, static_folder='./static')
 app.secret_key = 'marimari'
 
 # Ensure templates are auto-reloaded
@@ -50,13 +51,8 @@ def favorite():
     # likegoogleからデータを取得
     googles = db.execute("SELECT * FROM likegoogle WHERE user_id = ?", user_id)
 
-    # likeinstagramからデータを取得
-    instagrams = db.execute("SELECT * FROM likeinstagram WHERE user_id = ?", user_id)
 
-    # likeyoutubeからデータを取得
-    youtubes = db.execute("SELECT * FROM likeyoutube WHERE user_id = ?", user_id)
-
-    return render_template("favorite.html", googles=googles, instagrams=instagrams, youtubes=youtubes)
+    return render_template("favorite.html", googles=googles)
 
 # キーワードページ
 @app.route("/keyword", methods=["GET", "POST"])
@@ -162,47 +158,23 @@ def result():
     # 筋肉一覧
     MUSCLES = ["胸鎖乳突筋", "大胸筋", "上腕二頭筋", "前鋸筋", "外腹斜筋", "腹直筋", "内転筋群", "大腿四頭筋", "前脛骨筋", "僧帽筋", "三角筋", "広背筋", "前腕伸筋群", "前腕屈筋群", "下腿三頭筋", "棘下筋", "上腕三頭筋", "脊柱起立筋", "大腿筋", "ハムストリングス"]
     muscle = request.args.get("muscle")
-    if muscle not in MUSCLES:
-        return render_template("apology.html", msg="そのような筋肉はありません。")
 
-    else:
-        # Google、Instagram、YouTubeというテーブルから、その筋肉のデータを取得する
-        googles = db.execute("SELECT * FROM google WHERE muscle = ?", muscle)
-        instagrams = db.execute("SELECT * FROM instagram WHERE muscle = ?", muscle)
-        youtubes = db.execute("SELECT * FROM youtube WHERE muscle = ?", muscle)
-
-        # そのユーザーが既にお気に入り登録しているもののIDを取得する
-        googleids = db.execute("SELECT google_id from likegoogle WHERE user_id = ?", user_id)
-        instagramids = db.execute("SELECT instagram_id from likeinstagram WHERE user_id = ?", user_id)
-        youtubeids = db.execute("SELECT youtube_id from likeyoutube WHERE user_id = ?", user_id)
-        return render_template("result.html", googles=googles, instagrams=instagrams, youtubes=youtubes, googleids=googleids, instagramids=instagramids, youtubeids=youtubeids)
-
-@app.route('/likegoogle/<int:id>', methods=['POST', 'DELETE'])
-def toggle_likegoogle(id):
-    user_id = session["user_id"]
-    if request.method == 'POST':
-        db.execute("INSERT INTO likegoogle (user_id, google_id) VALUES(?, ?)", user_id, id)
-    elif request.method == 'DELETE':
-        db.execute("DELETE FROM likegoogle WHERE user_id = ? AND google_id = ?", user_id, id)
-    return '', 204
-
-@app.route('/likeinstagram/<int:id>', methods=['POST', 'DELETE'])
-def toggle_likeinstagram(id):
-    user_id = session["user_id"]
-    if request.method == 'POST':
-        db.execute("INSERT INTO likeinstagram (user_id, instagram_id) VALUES(?, ?)", user_id, id)
-    elif request.method == 'DELETE':
-        db.execute("DELETE FROM likeinstagram WHERE user_id = ? AND instagram_id = ?", user_id, id)
-    return '', 204
-
-@app.route('/likeyoutube/<int:id>', methods=['POST', 'DELETE'])
-def toggle_likeyoutube(id):
-    user_id = session["user_id"]
-    if request.method == 'POST':
-        db.execute("INSERT INTO likeyoutube (user_id, youtube_id) VALUES(?, ?)", user_id, id)
-    elif request.method == 'DELETE':
-        db.execute("DELETE FROM likeyoutube WHERE user_id = ? AND youtube_id = ?", user_id, id)
-    return '', 204
+    # Google、Instagram、YouTubeというテーブルから、その筋肉のデータを取得する
+    googles = db.execute("SELECT * FROM google WHERE muscle = ?", muscle)
+    instagrams = db.execute("SELECT * FROM instagram WHERE muscle = ?", muscle)
+    youtubes = db.execute("SELECT * FROM youtube WHERE muscle = ?", muscle)
+    return render_template("result.html", googles=googles, instagrams=instagrams, youtubes=youtubes)
 
 
+
+@app.route("/like", methods=["POST", "DELETE"])
+def like():
+    user_id =session["user_id"]
+    if request.method == "POST":
+        post_id = request.form["post_id"]
+        db.execute("INSERT INTO likegoogle(user_id, google_id) VALUES(?, ?)", user_id, post_id)
+    elif request.method == "DELETE":
+        post_id = request.form["post_id"]
+        db.execute("DELETE FROM likegoogle WHERE user_id = ? AND google_id = ?", user_id, post_id)
+    return "OK"
 
