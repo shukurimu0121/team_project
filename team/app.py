6a51f736f295845938f7919181a11fd6637acf20
shukurimu0121 <118036362+shukurimu0121@users.noreply.github.com>
2023-03-08 20:00:05 +0900

    ログイン機能のバグ修正

--- team/app.py
+++ team/app.py
@@ -5,7 +5,7 @@ from flask import Flask, flash, jsonify, redirect, render_template, request, ses
 from flask_session import Session
 from functools import wraps
 from werkzeug.security import check_password_hash, generate_password_hash
-import json
+from helpers import login_required
 
 # Configure application
 app = Flask(__name__, static_folder='./static')
@@ -21,20 +21,6 @@ db = SQL("sqlite:///team.db")
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
-
-# ログインを必須にする
-def login_required(f):
-    """
-    Decorate routes to require login.
-
-    https://flask.palletsprojects.com/en/1.1.x/patterns/viewdecorators/
-    """
-    @wraps(f)
-    def decorated_function(*args, **kwargs):
-        if session.get("user_id") is None:
-            return redirect("/login")
-        return f(*args, **kwargs)
-    return decorated_function
 
 @app.after_request
 def after_request(response):
